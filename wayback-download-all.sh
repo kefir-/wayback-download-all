1f590e4dc028e438ad3f78a3f7a58f417b04b5cf
Ketil Froyn <ketil@froyn.name>
2017-09-15 07:37:58 +0200

    Removed test sh

--- wayback-download-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-(
-  ts=$(date +%Y%m%d%H%M%S)
-  while read url; do
-    echo url="$url";
-    wayback_machine_downloader -e -l -t $ts "$url"
-  done
-)< <(echo http://www.friekaker.no/)
